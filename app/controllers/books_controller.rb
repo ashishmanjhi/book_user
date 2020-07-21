2190755024a042064887c398c153cc209d2e34f1
HMECL001029 <ashishmanjhi.97@gmail.com>
2020-07-21 19:04:37 +0530

    rubo

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,53 +1,50 @@
+# frozen_string_literal: true
+
 class BooksController < ApplicationController
-  before_action :find_book, only: [:show, :edit, :update, :destroy]
+  before_action :find_book, only: %i[show edit update destroy]
   before_action :authenticate_user!, except: [:index]
 
   def index
-  	 @books = Book.all.order("created_at DESC")
+    @books = Book.all.order('created_at DESC')
   end
 
   def new
-	@book = current_user.books.build
+    @book = current_user.books.build
   end
 
-  def show
-  end
+  def show; end
 
   def create
-		@book = current_user.books.build(book_params)
-		if @book.save
-			redirect_to root_path, notice: "Successfully created new Book"
-		else
-			render 'new'
-		end
-	end
-
-	def edit
-	end
-
-    def update
-        if @book.update(book_params)
-			redirect_to @book, notice: "Book was successfully updated"
-		else
-				render 'edit'
-		end
-				
+    @book = current_user.books.build(book_params)
+    if @book.save
+      redirect_to root_path, notice: 'Successfully created new Book'
+    else
+      render 'new'
     end
+  end
 
-    def destroy
-    	@book.destroy
-		redirect_to root_path
-    end
+  def edit; end
 
-    private
+  def update
+    if @book.update(book_params)
+      redirect_to @book, notice: 'Book was successfully updated'
+    else
+      render 'edit'
+  end
+  end
 
-	def book_params
-		params.require(:book).permit(:title, :publisher, :year, :place, :author, :description )
-	end
+  def destroy
+    @book.destroy
+    redirect_to root_path
+  end
 
-	def find_book
-		@book = Book.find(params[:id])
-	end
+  private
 
+  def book_params
+    params.require(:book).permit(:title, :publisher, :year, :place, :author, :description)
+  end
 
+  def find_book
+    @book = Book.find(params[:id])
+  end
 end
